ff314777e935f7e128fa8d9a68d5a11ecc7bd25e
mallikaramya14 <malliramya1410@gmail.com>
2019-05-21 21:22:43 -0700

    added a new file

---
+++ even.java
@@ -0,0 +1,16 @@
+class even
+{
+public static void main(String args[])
+{
+int x=10;
+if(x==2)
+{
+System.out.println("the numberr is even");
+}
+else
+{
+System.out.println("the number is odd");
+}
+}
+}
+
